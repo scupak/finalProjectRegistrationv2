7b5ad48e11de48618dbdcd1312f03caf43dba5bd
bhp <bhp@easv.dk>
2020-10-22 22:13:02 +0200

    AddStudent in StudentService + test

--- Services/StudentService.cs
+++ Services/StudentService.cs
@@ -1,4 +1,5 @@
 ﻿using Interfaces;
+using Model;
 using System;
 
 namespace Services
@@ -10,6 +11,34 @@ namespace Services
         public StudentService(IStudentRepository repo)
         {
             this.repo = repo ?? throw new ArgumentException("StudentRepository is missing");
+        }
+
+        public void AddStudent(Student student)
+        {
+            ThrowIfNotValidStudent(student);
+            if (repo.GetById(student.Id) != null)
+            {
+                throw new InvalidOperationException("Student already exist");
+            }
+            repo.Add(student);
+        }
+
+        private void ThrowIfNotValidStudent(Student student)
+        {
+            if (student == null)
+                throw new ArgumentException("Student is missing");
+            if (student.Id <= 0)
+                throw new ArgumentException("Invalid Student Property: Id");
+            if (student.Name == null || student.Name == "")
+                throw new ArgumentException("Invalid Student Property: Name");
+            if (student.Address == null || student.Address == "")
+                throw new ArgumentException("Invalid Student Property: Address");
+            if (student.ZipCode <= 0)
+                throw new ArgumentException("Invalid Student Property: ZipCode");
+            if (student.PostalDistrict == null || student.PostalDistrict == "")
+                throw new ArgumentException("Invalid Student Property: PostalDistrict");
+            if (student.Email == "")
+                throw new ArgumentException("Invalid Student Property: Email");
         }
     }
 }
--- XUnitTestProject/StudentServiceTest.cs
+++ XUnitTestProject/StudentServiceTest.cs
@@ -1,7 +1,9 @@
 using Interfaces;
+using Model;
 using Moq;
 using Services;
 using System;
+using System.Net.Sockets;
 using Xunit;
 
 namespace XUnitTestProject
@@ -15,27 +17,29 @@ namespace XUnitTestProject
             repoMock = new Mock<IStudentRepository>();
         }
 
+        #region CreateStudentService
+
         [Fact]
-        public void CreateStudentServiceWithValidRepository()
+        public void CreateStudentService_WithValidRepository()
         {
             // arrange
             var repo = repoMock.Object;
 
             // act
             var service = new StudentService(repo);
-            
+
             // assert
             Assert.NotNull(service);
         }
 
         [Fact]
-        public void CreateStudentServiceRepositoryIsNullExpectArgumentException()
+        public void CreateStudentService_RepositoryIsNull_ExpectArgumentException()
         {
             // arrange
             StudentService service = null;
 
             // act + assert
-            var ex = Assert.Throws<ArgumentException>(() => 
+            var ex = Assert.Throws<ArgumentException>(() =>
             {
                 service = new StudentService(null);
             });
@@ -43,5 +47,114 @@ namespace XUnitTestProject
             Assert.Equal("StudentRepository is missing", ex.Message);
             Assert.Null(service);
         }
+
+        #endregion
+
+        #region AddStudent
+
+        [Theory]
+        [InlineData(1, "Name", "Address", 1234, "PostalDistrict", "e@mail.dk")]
+        [InlineData(1, "Name", "Address", 1234, "PostalDistrict", null)]
+        public void AddStudent_ValidStudentNonExisting(int id, string name, string address, int zipcode, string postalDistrict, string email)
+        {
+            // arrange
+            Student student = new Student()
+            {
+                Id = id,
+                Name = name,
+                Address = address,
+                ZipCode = zipcode,
+                PostalDistrict = postalDistrict,
+                Email = email
+            };
+
+            // make sure the student does not exist before test
+            repoMock.Setup(repo => repo.GetById(It.Is<int>(z => z == student.Id))).Returns(() => null);
+
+            StudentService service = new StudentService(repoMock.Object);
+
+            // act
+            service.AddStudent(student);
+
+            // assert
+            repoMock.Verify(repo => repo.Add(It.Is<Student>(s => s == student)), Times.Once);
+        }
+
+        [Fact]
+        public void AddStudent_StudentIsNull_ExpectArgumentException()
+        {
+            // arrange
+            var service = new StudentService(repoMock.Object);
+
+            // act + assert
+            var ex = Assert.Throws<ArgumentException>(() => service.AddStudent(null));
+
+            Assert.Equal("Student is missing", ex.Message);
+            repoMock.Verify(repo => repo.Add(It.Is<Student>(s => s == null)), Times.Never);
+        }
+
+        [Theory]
+        [InlineData(0, "Name", "Address", 1234, "PostalDistrict", "e@mail.dk", "Id")]     // invalid Id = 0
+        [InlineData(-1, "Name", "Address", 1234, "PostalDistrict", "e@mail.dk", "Id")]    // invalid Id = -1
+        [InlineData(1, null, "Address", 1234, "PostalDistrict", "e@mail.dk", "Name")]     // Name is null
+        [InlineData(1, "", "Address", 1234, "PostalDistrict", "e@mail.dk", "Name")]       // Name is empty
+        [InlineData(1, "Name", null, 1234, "PostalDistrict", "e@mail.dk", "Address")]     // Address is null
+        [InlineData(1, "Name", "", 1234, "PostalDistrict", "e@mail.dk", "Address")]       // Address is empty
+        [InlineData(1, "Name", "Address", 0, "PostalDistrict", "e@mail.dk", "ZipCode")]   // invalid ZipCode = 0
+        [InlineData(1, "Name", "Address", -1, "PostalDistrict", "e@mail.dk", "ZipCode")]  // invalid ZipCode = -1
+        [InlineData(1, "Name", "Address", 1234, null, "e@mail.dk", "PostalDistrict")]     // PostalDistrict is null
+        [InlineData(1, "Name", "Address", 1234, "", "e@mail.dk", "PostalDistrict")]       // PostalDistrict is empty
+        [InlineData(1, "Name", "Address", 1234, "PostalDistrict", "", "Email")]           // Email is empty
+        public void AddStudent_InvalidStudent_ExpectArgumentException(int id, string name, string address, int zipcode, string postalDistrict, string email, string field)
+        {
+            // arrange
+            Student student = new Student()
+            {
+                Id = id,
+                Name = name,
+                Address = address,
+                ZipCode = zipcode,
+                PostalDistrict = postalDistrict,
+                Email = email
+            };
+
+            // make sure the student does not exist before test
+            repoMock.Setup(repo => repo.GetById(It.Is<int>(z => z == student.Id))).Returns(() => null);
+
+            StudentService service = new StudentService(repoMock.Object);
+
+            // act + assert
+            var ex = Assert.Throws<ArgumentException>(() => service.AddStudent(student));
+
+            Assert.Equal($"Invalid Student Property: {field}", ex.Message);
+            repoMock.Verify(repo => repo.Add(It.Is<Student>(s => s == student)), Times.Never);
+        }
+
+        [Fact]
+        public void AddStudent_StudentExists_ExpectInvalidOperationException()
+        {
+            // arrange
+            Student student = new Student()
+            {
+                Id = 1,
+                Name = "name",
+                Address = "address",
+                ZipCode = 1234,
+                PostalDistrict = "postalDistrict",
+                Email = "email"
+            };
+
+            // make sure the student exists before test
+            repoMock.Setup(repo => repo.GetById(It.Is<int>(z => z == student.Id))).Returns(() => student);
+
+            StudentService service = new StudentService(repoMock.Object);
+
+            // act + assert
+            var ex = Assert.Throws<InvalidOperationException>(() => service.AddStudent(student));
+
+            Assert.Equal("Student already exist", ex.Message);
+            repoMock.Verify(repo => repo.Add(It.Is<Student>(s => s == student)), Times.Never);
+        }
+        #endregion
     }
 }
