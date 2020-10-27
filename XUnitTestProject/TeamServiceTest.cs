f63ce71ad8f992c07c7907fefb6c985c0816a1fa
bhp <bhp@easv.dk>
2020-10-27 21:25:44 +0100

    Implemented AddTeam in TeamService + Tests

--- XUnitTestProject/TeamServiceTest.cs
+++ XUnitTestProject/TeamServiceTest.cs
@@ -1,7 +1,9 @@
 ﻿using Interfaces;
+using Model;
 using Moq;
 using Services;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace XUnitTestProject
@@ -15,6 +17,7 @@ namespace XUnitTestProject
             repoMock = new Mock<ITeamRepository>();
         }
 
+        #region CreateTeamService
         [Fact]
         public void CreateTeamService_ValidRepository()
         {
@@ -40,5 +43,108 @@ namespace XUnitTestProject
             Assert.Equal("Team Repository is missing", ex.Message);
             Assert.Null(service);
         }
+
+        #endregion
+
+        #region AddTeam
+
+        [Fact]
+        public void AddTeam_NonExistingValidTeam()
+        {
+            // arrange
+            Team t = new Team()
+            {
+                Id = 1,
+                Students = new List<Student>()
+            };
+
+            // Team t does not exist in the TeamRepository
+            repoMock.Setup(repo => repo.GetById(It.Is<int>(id => id == t.Id))).Returns(() => null);
+
+            TeamService service = new TeamService(repoMock.Object);
+
+            // act
+            service.AddTeam(t);
+
+            // assert
+            repoMock.Verify(repo => repo.Add(It.Is<Team>(team => team == t)), Times.Once);
+        }
+
+        [Fact]
+        public void AddTeam_TeamExists_ExpectInvalidOperationException()
+        {
+            // arrange
+            Team t = new Team()
+            {
+                Id = 1,
+                Students = new List<Student>()
+            };
+
+            // Team t already exists in the TeamRepository
+            repoMock.Setup(repo => repo.GetById(It.Is<int>(id => id == t.Id))).Returns(() => t);
+
+            TeamService service = new TeamService(repoMock.Object);
+
+            // act + assert
+            var ex = Assert.Throws<InvalidOperationException>(() => service.AddTeam(t));
+
+            Assert.Equal("Team already in Repository", ex.Message);
+            repoMock.Verify(repo => repo.Add(It.Is<Team>(team => team == t)), Times.Never);
+        }
+
+        [Fact]
+        public void AddTeam_TeamIsNull_ExpectArgumentException()
+        {
+            // arrange
+            TeamService service = new TeamService(repoMock.Object);
+
+            // act + assert
+            var ex = Assert.Throws<ArgumentException>(() => service.AddTeam(null));
+
+            Assert.Equal("Team is missing", ex.Message);
+            repoMock.Verify(repo => repo.Add(It.Is<Team>(team => team == null)), Times.Never);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void AddTeam_InvalidTeamId_ExpectArgumentException(int teamId)
+        {
+            // arrange
+            Team t = new Team()
+            {
+                Id = teamId,
+                Students = new List<Student>()
+            };
+
+            TeamService service = new TeamService(repoMock.Object);
+
+            // act + assert
+            var ex = Assert.Throws<ArgumentException>(() => service.AddTeam(t));
+
+            Assert.Equal("Invalid Team Id", ex.Message);
+            repoMock.Verify(repo => repo.Add(It.Is<Team>(team => team == t)), Times.Never);
+        }
+
+        [Fact]
+        public void AddTeam_InvalidStudentsList_ExpectArgumentException()
+        {
+            // arrange
+            Team t = new Team()
+            {
+                Id = 1,
+                Students = null
+            };
+
+            TeamService service = new TeamService(repoMock.Object);
+
+            // act + assert
+            var ex = Assert.Throws<ArgumentException>(() => service.AddTeam(t));
+
+            Assert.Equal("Invalid Students property", ex.Message);
+            repoMock.Verify(repo => repo.Add(It.Is<Team>(team => team == t)), Times.Never);
+        }
+
+        #endregion
     }
 }
