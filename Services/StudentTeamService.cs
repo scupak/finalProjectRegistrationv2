5b757f1e260e8fd2bb3a64685d4413843f143d15
bhp <bhp@easv.dk>
2020-10-29 20:52:17 +0100

    Implemented constructor for StudentTeamService + test

---
+++ Services/StudentTeamService.cs
@@ -0,0 +1,19 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class StudentTeamService
+    {
+        private readonly IStudentRepository StudentsRepo;
+        private readonly ITeamRepository TeamsRepo;
+
+        public StudentTeamService(IStudentRepository studentRepository, ITeamRepository teamRepository)
+        {
+            this.StudentsRepo = studentRepository ?? throw new ArgumentException("StudentRepository is missing");
+            this.TeamsRepo = teamRepository ?? throw new ArgumentException("TeamRepository is missing");
+        }
+    }
+}
