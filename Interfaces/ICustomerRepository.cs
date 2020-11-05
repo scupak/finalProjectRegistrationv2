59290fe5479a3e2a1e0dd66192c5c6b1812d3f87
bhp <bhp@easv.dk>
2020-11-05 21:37:03 +0100

    Implemented constructor for CustomerService + test

---
+++ Interfaces/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Model;
+using System.Collections.Generic;
+
+namespace Interfaces
+{
+    public interface ICustomerRepository
+    {
+        void Add(Customer c);
+        void Update(Customer c);
+        void Remove(Customer c);
+        IEnumerable<Customer> GetAll();
+        Customer GetById(int id);
+    }
+}
---
+++ Services/CustomerService.cs
@@ -0,0 +1,19 @@
+﻿using Interfaces;
+using System;
+
+namespace Services
+{
+    public class CustomerService
+    {
+        private readonly ICustomerRepository customerRepository;
+
+        public CustomerService(ICustomerRepository repo)
+        {
+            if (repo == null)
+            {
+                throw new ArgumentException("Customer Repository is missing");
+            }
+            customerRepository = repo;
+        }
+    }
+}
