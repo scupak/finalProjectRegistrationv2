82de9c388be558c1fc7b71d34fbb98fa234d44fa
bhp <bhp@easv.dk>
2020-11-05 23:31:53 +0100

    Renamed - CustomerService to CompanyService - CustomerServiceTest to CompanyServiceTest - Customer to Company - ICustomerRepository to ICompanyRepository

---
+++ Services/CompanyService.cs
@@ -0,0 +1,46 @@
+﻿using Interfaces;
+using Model;
+using System;
+
+namespace Services
+{
+    public class CompanyService
+    {
+        private readonly ICompanyRepository companyRepository;
+
+        public CompanyService(ICompanyRepository repo)
+        {
+            companyRepository = repo ?? throw new ArgumentException("Company Repository is missing");
+        }
+
+        public void AddCompany(Company company)
+        {
+            if (company == null)
+            {
+                throw new ArgumentException("Company is missing");
+            }
+            if (!IsValidCompany(company))
+            {
+                throw new ArgumentException("Invalid Company property");
+            }
+            if (companyRepository.GetById(company.Id) != null)
+            {
+                throw new InvalidOperationException("Company already exists");
+            }
+
+            companyRepository.Add(company);
+        }
+
+        private bool IsValidCompany(Company c)
+        {
+            return
+                !(c.Id <= 0
+                || String.IsNullOrEmpty(c.Name)
+                || String.IsNullOrEmpty(c.Address)
+                || c.Zipcode <= 0
+                || String.IsNullOrEmpty(c.PostalDistrict)
+                || c.CompanyUri == ""
+                || c.Projects == null);
+        }
+    }
+}
