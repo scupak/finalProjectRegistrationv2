81bde11a4ac04f18e72db91085d377048c9d6983
bhp <bhp@easv.dk>
2020-11-05 22:53:57 +0100

    Implemented AddCustomer + test

--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -1,4 +1,5 @@
 ﻿using Interfaces;
+using Model;
 using System;
 
 namespace Services
@@ -14,6 +15,35 @@ namespace Services
                 throw new ArgumentException("Customer Repository is missing");
             }
             customerRepository = repo;
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            if (customer == null)
+            {
+                throw new ArgumentException("Customer is missing");
+            }
+            if (!IsValidCustomer(customer))
+            {
+                throw new ArgumentException("Invalid Customer property");
+            }
+            if (customerRepository.GetById(customer.Id) != null)
+            {
+                throw new InvalidOperationException("Customer already exists");
+            }
+
+            customerRepository.Add(customer);
+        }
+
+        private bool IsValidCustomer(Customer c)
+        {
+            return
+                !(c.Id <= 0
+                || String.IsNullOrEmpty(c.Name)
+                || String.IsNullOrEmpty(c.Address)
+                || c.Zipcode <= 0
+                || String.IsNullOrEmpty(c.PostalDistrict)
+                || c.CompanyUri == "");
         }
     }
 }
--- XUnitTestProject/CustomerServiceTest.cs
+++ XUnitTestProject/CustomerServiceTest.cs
@@ -14,8 +14,6 @@ namespace XUnitTestProject
         private SortedDictionary<int, Customer> allCustomers;
         private Mock<ICustomerRepository> repoMock;
 
-        #region CreateCustomerService
-
         public CustomerServiceTest()
         {
             allCustomers = new SortedDictionary<int, Customer>();
@@ -27,6 +25,8 @@ namespace XUnitTestProject
             repoMock.Setup(repo => repo.GetById(It.IsAny<int>())).Returns<int>((id) => allCustomers.ContainsKey(id) ? allCustomers[id] : null);
         }
 
+
+        #region CreateCustomerService
         [Fact]
         public void CreateCustomerService_ValidCustomerRepository()
         {
@@ -47,9 +47,117 @@ namespace XUnitTestProject
             CustomerService service = null;
 
             // act + assert
-            var ex = Assert.Throws<ArgumentException>(() => service = new CustomerService(null)); 
+            var ex = Assert.Throws<ArgumentException>(() => service = new CustomerService(null));
 
             Assert.Equal("Customer Repository is missing", ex.Message);
+        }
+
+        #endregion
+
+        #region AddCustomer
+
+        [Theory]
+        [InlineData(1, "Company", "Address", 1234, "District", "uri")]
+        [InlineData(1, "Company", "Address", 1234, "District", null)]
+        public void AddCustomer_ValidCustomer(int id, string name, string address, int zipcode, string district, string uri)
+        {
+            // arrange
+            var customer = new Customer()
+            {
+                Id = id,
+                Name = name,
+                Address = address,
+                Zipcode = zipcode,
+                PostalDistrict = district,
+                CompanyUri = uri,
+                Projects = new List<Project>()
+            };
+
+            CustomerService service = new CustomerService(repoMock.Object);
+
+            // act
+            service.AddCustomer(customer);
+
+            // assert
+            Assert.Contains(customer, allCustomers.Values);
+            repoMock.Verify(repo => repo.Add(It.Is<Customer>(c => c == customer)), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(0, "Company", "Address", 1234, "District", "uri")]      // invalid id: 0
+        [InlineData(-1, "Company", "Address", 1234, "District", "uri")]     // invalid id: -1
+        [InlineData(1, null, "Address", 1234, "District", "uri")]           // invalid name: null
+        [InlineData(1, "", "Address", 1234, "District", "uri")]             // invalid name: ""
+        [InlineData(1, "Company", null, 1234, "District", "uri")]           // invalid address: null
+        [InlineData(1, "Company", "", 1234, "District", "uri")]             // invalid address: ""
+        [InlineData(1, "Company", "Address", 0, "District", "uri")]         // invalid zipcode: 0
+        [InlineData(1, "Company", "Address", -1, "District", "uri")]        // invalid zipcode: -1
+        [InlineData(1, "Company", "Address", 1234, null, "uri")]            // invalid PostalDistrict: null
+        [InlineData(1, "Company", "Address", 1234, "", "uri")]              // invalid PostalDistrict: ""
+        [InlineData(1, "Company", "Address", 1234, "District", "")]         // invaild CompanyUri: ""
+        public void AddCustomer_InvalidCustomer(int id, string name, string address, int zipcode, string district, string uri)
+        {
+            // arrange
+            var customer = new Customer()
+            {
+                Id = id,
+                Name = name,
+                Address = address,
+                Zipcode = zipcode,
+                PostalDistrict = district,
+                CompanyUri = uri,
+                Projects = new List<Project>()
+            };
+
+            CustomerService service = new CustomerService(repoMock.Object);
+
+            // act + assert
+            var ex = Assert.Throws<ArgumentException>(() => service.AddCustomer(customer));
+
+            Assert.Equal("Invalid Customer property", ex.Message);
+            repoMock.Verify(repo => repo.Add(It.Is<Customer>(c => c == customer)), Times.Never);
+        }
+
+        [Fact]
+        public void AddCustomer_CustomerIsNUll_ExpectArgumentException()
+        {
+            // arrange
+            CustomerService service = new CustomerService(repoMock.Object);
+
+            // act + assert
+            var ex = Assert.Throws<ArgumentException>(() => service.AddCustomer(null));
+
+            Assert.Equal("Customer is missing", ex.Message);
+            repoMock.Verify(repo => repo.Add(It.Is<Customer>(c => c == null)), Times.Never);
+        }
+
+        [Fact]
+        public void AddCustomer_CustomerAlreadyExists_InvalidOperationException()
+        {
+            // arrange
+            
+            var repo = repoMock.Object;
+
+            // customer exists in customer repository
+            var customer = new Customer()
+            {
+                Id = 1,
+                Name = "Company",
+                Address = "Address",
+                Zipcode = 1234,
+                PostalDistrict = "District",
+                CompanyUri = "companyUrl"
+            };
+            allCustomers.Add(customer.Id, customer);
+
+            var service = new CustomerService(repo);
+
+            // act + assert
+
+            var ex = Assert.Throws<InvalidOperationException>(() => service.AddCustomer(customer));
+
+            Assert.Equal("Customer already exists", ex.Message);
+            repoMock.Verify(repo => repo.Add(It.Is<Customer>(c => c == customer)), Times.Never);
         }
 
         #endregion
